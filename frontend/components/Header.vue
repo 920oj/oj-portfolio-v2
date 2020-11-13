df6a29d0928e9642a8c7c6539755a1ea22d59e10
Junya Oiwa <0920oiwa@gmail.com>
2020-11-13 13:55:27 +0900

    [add]ヘッダーを作成

---
+++ frontend/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header">
+    <div class="logo-line flex justify-center align-center w-100">
+      <img class="w-32 my-8" src="/img/logo-black.png" alt="920oj.net" />
+    </div>
+    <div class="menu-line">
+      <ul class="flex justify-center align-center">
+        <li class="mx-8">home</li>
+        <li class="mx-8">about</li>
+        <li class="mx-8">works</li>
+        <li class="mx-8">blog</li>
+        <li class="mx-8">contact</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+export default {}
+</script>
